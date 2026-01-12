f9532371207141e40dbb08eacb65711f4c628d44
Rafi Strauss <rafi.strauss@nbcuni.com>
2026-01-11 20:11:31 -0500

    Update favicon to new design

--- sveltekit-app/src/routes/+page.svelte
+++ sveltekit-app/src/routes/+page.svelte
@@ -311,7 +311,7 @@
 
 <svelte:head>
 	<title>D&D Character Sheet</title>
-	<link rel="icon" type="image/png" href="favicon.png">
+	<link rel="icon" type="image/png" href="favicon4.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </svelte:head>
 
